e218f89ad004e305ceae44e92e33e1695f1a851d
gabzzz-dias <gabriel.menezes.dias@hotmail.com>
2021-11-15 08:14:52 -0300

    add thank notes

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.use('/login', LoginRouter);
 app.use('/post', PostRouter);
 app.use('/categories', CategoryRouter);
 
+// Agradecimentos especiais ao meu amigo de turma Leandro Reis, turma 10-B, que me ajudou no entendimento de alguns pontos importantes no funcionamento do projeto, me permitindo olhar sua PR, com a qual, mesmo com dificuldade, consegui concluir o projeto. Obrigado, Leandro!
+// PR do Leandro: https://github.com/tryber/sd-010-b-project-blogs-api/pull/52
+
 app.get('/', (request, response) => {
   response.send();
 });
